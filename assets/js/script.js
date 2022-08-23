c35ff180d81daed34d8a56b7dca433d8aabb2694
Lo0syash <arturdzhan2004@gmail.com>
2022-08-23 22:15:03 +0300

    add

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+function visible(){
+    var as = document.querySelector('.loader-first');
+    as.style.display = 'block';
+}
+
+function visible_second(){
+
+}
+
+function visible_third(){
+
+}
+
+function visible_four(){
+
+}
+
+function visible_five(){
+
+}
