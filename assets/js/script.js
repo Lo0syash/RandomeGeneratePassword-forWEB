e9a4060d7042d454e66987ed184ff6edaa263352
Lo0syash <arturdzhan2004@gmail.com>
2022-10-22 01:37:27 +0300

    add copy, generate and length

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,62 @@ else{
 }
 
 
+const num = document.querySelector('.num');
+const symbols = document.querySelector('.symbols');
+const upper = document.querySelector('.upper');
+const lower = document.querySelector('.lower');
+
+const out = document.querySelector(".output");
+
+const range = document.querySelector('.range-lenth');
+
+if(range){
+  range.addEventListener("input", () =>{
+    const numer = document.querySelector(".num-range");
+    numer.innerHTML = range.value;
+  })
+}
+
+const btn_generate = document.querySelector('.icon-generate').addEventListener('click', () =>{
+  var result = "";
+  const out = document.querySelector('.textbox');
+  if (upper.checked) {
+      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  } else if (lower.checked) {
+      var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  }  else if (num.checked) {
+      var characters = "0123456789";
+  } else if (symbols.checked) {
+      var characters = "!@#$%^&*()_+=-/?.,><:;{}[]|//`~№";
+  } else {
+      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  }
+    var charactersLength = characters.length;
+  for (var i = 0; i < range.value; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  
+  out.innerHTML = result;
+  console.log(result);
+});
+
+
+//---------------------------------copy password
+const copy = document.querySelector(".btn-copy");
+
+if (copy) {
+  copy.onclick = () => {
+    var textCopy = document.querySelector(".textbox");
+    if (textCopy.innerHTML != "") {
+      navigator.clipboard.writeText(textCopy.innerHTML);
+      const advert = document.querySelector(".advert");
+      advert.classList.add("active");
+      setTimeout(() => advert.classList.remove("active"), 1000);
+    } else {
+      alert("first generate!");
+    }
+  };
+}
 
 
 
@@ -30,6 +86,30 @@ else{
 
 
 
+
+
+
+
+
+// function generate(){
+//   var result = "";
+//   if (upper.checked) {
+//     var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//   } else if (lower.checked) {
+//     var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+//   }  else if (num.checked) {
+//     var characters = "0123456789";
+//   } else if (symbols.checked) {
+//     var characters = "!@#$%^&*()_+=-/?.,><:;{}[]|//`~№";
+//   } else {
+//     var characters =
+//       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   }
+//   var charactersLength = characters.length;
+//   for (var i = 0; i < range.value; i++) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   }
+// }
 
 
 // let a, b, c, d, e;
